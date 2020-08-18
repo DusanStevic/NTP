0614612396fd035b2d4ccb0b303d350f5a58e50a
Dusan Stevic <stevicdule@gmail.com>
2020-08-18 10:57:40 +0200

    python finance serial version

--- Python/MonteCarloSimulationFinance.py
+++ Python/MonteCarloSimulationFinance.py
@@ -7,14 +7,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pandas_datareader import data
 from scipy.stats import norm
+import copy
 
 
 class MonteCarloSimulationFinance:
-    def __init__(self, number_of_processes):
+    def __init__(self, start_date, end_date, ticker_symbol, number_of_processes):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.ticker_symbol = ticker_symbol
+        self.data = None
+        self.time_series = None
         self.number_of_processes = number_of_processes
 
+    def data_acquisition(self):
+        stocks = data.DataReader(self.ticker_symbol, 'yahoo', self.start_date, self.end_date)
+        stocks = stocks.dropna()
+        self.time_series = stocks['Close']
+
+
     def calculate_periodic_daily_return(self):
-        return 4
+        self.data= np.log(self.time_series).diff().dropna()
 
     # A z-table, also called the standard normal table, is a mathematical table that allows us to know
     # the percentage of values below (to the left) a z-score in a standard normal distribution (SND).
@@ -24,33 +36,37 @@ class MonteCarloSimulationFinance:
     def calculate_z_score(self):
         return norm.ppf(random.random())
 
-    def calculate_average_daily_return(self,data):
-        return np.mean(data)
+    def calculate_average_daily_return(self):
+        return np.mean(self.data)
 
-    def calculate_variance(self,data):
-        return np.var(data)
+    def calculate_variance(self):
+        return np.var(self.data)
 
-    def calculate_standard_deviation(self,data):
-        return np.std(data)
+    def calculate_standard_deviation(self):
+        return np.std(self.data)
 
-    def calculate_drift(self,data):
-        return self.calculate_average_daily_return(data) - self.calculate_variance(data) / 2
+    def calculate_drift(self):
+        return self.calculate_average_daily_return() - self.calculate_variance() / 2
 
-    def calculate_random_value(self,data):
-        return self.calculate_standard_deviation(data) * self.calculate_z_score()
+    def calculate_random_value(self):
+        return self.calculate_standard_deviation() * self.calculate_z_score()
 
     # prediction window size: number of prediction days per simulation
-    def simulation_finance(self,number_of_simulations,prediction_window_size):
+    def simulation_finance(self, number_of_simulations, prediction_window_size):
         predictions = []
-        for x in range(number_of_simulations):
-            x = random.random()
-            y = random.random()
-            if x * x + y * y < 1:
-                inside = inside + 1
+        prediction = []
+        for i in range(number_of_simulations):
+            # today’s price
+            prediction.append(self.time_series.iloc[-1])
+            for j in range(prediction_window_size):
+                #Next Day’s Price=Today’s Price × e^(Drift+Random Value)
+                prediction.append(prediction[-1] * pow(math.e, (self.calculate_drift() + self.calculate_random_value())))
 
-        return inside
+            predictions.append(copy.deepcopy(prediction))
+            prediction.clear()
+        return predictions
 
-    def mcs_finance_serial(self,number_of_simulations):
+    def mcs_finance_serial(self, number_of_simulations):
         pass
 
     def mcs_finance_parallel(self, number_of_simulations):
@@ -58,64 +74,12 @@ class MonteCarloSimulationFinance:
 
 
 if __name__ == "__main__":
-    # OVO SLJAKA
-    # outFileName=r"C:\Users\Dule\Desktop\NAPREDNE TEHNIKE PROGRAMIRANJA\PROJEKAT\NTP\Pharo\MonteCarloSimulationPi.txt"
-    # outFile=open(outFileName, "w")
-    # for i in range(100):
-    #     x = random.randint(0, 500)
-    #     y = random.randint(0, 500)
-    #     print(str(x)+' '+str(y))
-    #     outFile.write(str(x)+' '+str(y)+'\n')
-    # outFile.close()
 
-    monte_carlo_simulation_finance = MonteCarloSimulationFinance(5)
-    start = time.time()
-    # print("Approximation of Pi by using the Monte Carlo simulation serial version:" + str(
-    # monte_carlo_simulation_pi.mcs_pi_serial(10000000)))
-    #print("Approximation of Pi by using the Monte Carlo simulation parallel version:" + str(
-        #monte_carlo_simulation_pi.mcs_pi_parallel(10000000)))
-    end = time.time()
-    duration = end - start
-    print(f"Duration {duration} seconds")
+    m = MonteCarloSimulationFinance('1980-01-01', '2019-12-31', 'AAPL', 5)
+    m.data_acquisition()
+    m.calculate_periodic_daily_return()
+    print(m.simulation_finance(3, 5))
 
-    start_date = '1980-01-01'
-    end_date = '2019-12-31'
-    stocks = data.DataReader('AAPL', 'yahoo', start_date, end_date)
-    stocks = stocks.dropna()
-    time_series = stocks['Close']
-    # print(time_series)
-    l = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    rez = []
-    for index in range(1, len(l)):
-        rez.append(math.log(l[index] / l[index - 1]))
 
-    l = pd.DataFrame(l)
-    log = np.log(time_series).diff()
-    print(rez)
-    print(log)
 
-    #print(calculate_average_daily_return(log))
-    #print('ovo je varijansa:' + str(calculate_variance(log)))
-    #print('ovo je standardna devijacija:' + str(calculate_standard_deviation(log)))
 
-    #drift = calculate_drift(log)
-    #random_value = calculate_random_value(log)
-
-    print(time_series.tail(30))
-
-    # time_series.plot(figsize=(20, 10))
-    # plt.title('Closing stock price')
-    # plt.ylabel('Stock Price($)')
-    # plt.xlabel('Date')
-    # plt.legend(loc='upper right')
-    # plt.show()
-
-    # print(time_series.iloc[-1])
-    # print(time_series.iloc[-2]*pow(math.e, (drift+random_value)))
-    predictions = []
-    predictions.append(time_series.iloc[-30])
-    # 30 days prediction
-    #for i in range(30):
-     #   predictions.append(predictions[-1] * pow(math.e, (drift + random_value)))
-    #for i in predictions:
-     #   print(i)
