5e1b1363b0e96e16bb54f70b8e7906916d54b34f
Dusan Stevic <stevicdule@gmail.com>
2020-08-25 09:41:32 +0200

    z score in golang

--- Go/MonteCarloSimulationFinance/MonteCarloSimulationFinance.go
+++ Go/MonteCarloSimulationFinance/MonteCarloSimulationFinance.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"reflect"
+	"time"
 
+	"github.com/chobie/go-gaussian"
 	"github.com/markcheno/go-quote"
 	"gonum.org/v1/gonum/stat"
 )
@@ -22,6 +25,35 @@ func (monteCarloSimulationFinance *MonteCarloSimulationFinance) dataAcquisition(
 	stock, _ := quote.NewQuoteFromYahoo(monteCarloSimulationFinance.tickerSymbol,
 		monteCarloSimulationFinance.startDate, monteCarloSimulationFinance.endDate, quote.Daily, true)
 	monteCarloSimulationFinance.timeSeries = stock.Close
+}
+
+/*  A z-table, also called the standard normal table, is a mathematical table that allows us to know
+the percentage of values below (to the left) a z-score in a standard normal distribution (SND).
+A z-score, also known as a standard score, indicates the number of standard deviations
+a raw score lays above or below the mean. When the mean of the z-score is calculated it is always 0,
+and the standard deviation (variance) is always in increments of 1. */
+func (monteCarloSimulationFinance *MonteCarloSimulationFinance) calculateZScore() float64 {
+	normalDistribution := gaussian.NewGaussian(0, 1)
+	/* 	Source code for random number generator https://play.golang.org/p/ZdFpbahgC1
+	   	The default number generator is deterministic, so it'll
+	   	produce the same sequence of numbers each time by default.
+	   	To produce varying sequences, give it a seed that changes.
+	   	Note that this is not safe to use for random numbers you
+	   	intend to be secret, use `crypto/rand` for those.
+	   	Seeding - Go provides a method, Seed(seed int64), that allows you
+	   	to initialize this default sequence. Implementation is slow
+	   	to make it faster rand.Seed(time.Now().UnixNano()) is added.
+	   	Seed is the current time, converted to int64 by UnixNano.
+	   	Gives constantly changing numbers. */
+
+	// Seed
+	s := rand.NewSource(time.Now().UnixNano())
+	// Randomly changing numbers.
+	r := rand.New(s)
+	/* 	Call the resulting `rand.Rand` just like the
+	   	functions on the `rand` package. */
+	return normalDistribution.Ppf(r.Float64())
+
 }
 
 func main() {
@@ -58,5 +90,5 @@ func main() {
 	fmt.Printf("std-dev=  %v\n", stddev)
 	fmt.Println(reflect.TypeOf(stock.Close))
 
-	fmt.Println(monteCarloSimulationFinance.timeSeries)
+	fmt.Println(monteCarloSimulationFinance.calculateZScore())
 }
