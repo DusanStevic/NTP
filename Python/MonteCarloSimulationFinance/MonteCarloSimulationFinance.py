6f655638cd5c6d4a324fe0b747a6171c531feac8
Dusan Stevic <stevicdule@gmail.com>
2020-09-01 09:12:08 +0200

    python export finance files

--- Python/MonteCarloSimulationFinance/MonteCarloSimulationFinance.py
+++ Python/MonteCarloSimulationFinance/MonteCarloSimulationFinance.py
@@ -17,6 +17,7 @@ class MonteCarloSimulationFinance:
         self.data = None
         self.time_series = None
         self.number_of_processes = number_of_processes
+        self.parallel_flag = False
 
     def data_acquisition(self):
         stock = data.DataReader(self.ticker_symbol, 'yahoo', self.start_date, self.end_date)
@@ -27,9 +28,9 @@ class MonteCarloSimulationFinance:
     def calculate_periodic_daily_return(self):
         # Differencing time series
         # The diff() function calculates the first differences of the time series.
-        self.data= np.log(self.time_series).diff().dropna()
+        self.data = np.log(self.time_series).diff().dropna()
         # Shifting and lagging time series
-        #self.data=np.log(self.time_series / self.time_series.shift(1)).dropna()
+        # self.data=np.log(self.time_series / self.time_series.shift(1)).dropna()
 
     # A z-table, also called the standard normal table, is a mathematical table that allows us to know
     # the percentage of values below (to the left) a z-score in a standard normal distribution (SND).
@@ -62,17 +63,20 @@ class MonteCarloSimulationFinance:
             # today’s price
             prediction.append(self.time_series.iloc[-1])
             for j in range(prediction_window_size):
-                #Next Day’s Price=Today’s Price × e^(Drift+Random Value)
-                prediction.append(prediction[-1] * pow(math.e, (self.calculate_drift() + self.calculate_random_value())))
+                # Next Day’s Price=Today’s Price × e^(Drift+Random Value)
+                prediction.append(
+                    prediction[-1] * pow(math.e, (self.calculate_drift() + self.calculate_random_value())))
 
             predictions.append(copy.deepcopy(prediction))
             prediction.clear()
         return predictions
 
-    def mcs_finance_serial(self, number_of_simulations,prediction_window_size):
-        return self.simulation_finance(number_of_simulations,prediction_window_size)
+    def mcs_finance_serial(self, number_of_simulations, prediction_window_size):
+        self.parallel_flag = False
+        return self.simulation_finance(number_of_simulations, prediction_window_size)
 
-    def mcs_finance_parallel(self, number_of_simulations,prediction_window_size):
+    def mcs_finance_parallel(self, number_of_simulations, prediction_window_size):
+        self.parallel_flag = True
         pool = Pool(processes=self.number_of_processes)
         number_of_simulations_per_process = int(number_of_simulations / self.number_of_processes)
         simulations_per_process = []
@@ -81,10 +85,54 @@ class MonteCarloSimulationFinance:
         # l += n * [v]
         # Mapping a function with multiple arguments to a multiprocessing pool will distribute
         # the input data across processes to be run with the referenced function.
-        simulations_per_process += self.number_of_processes * [(number_of_simulations_per_process,prediction_window_size)]
-        rez = pool.starmap(self.simulation_finance, simulations_per_process)
-        return rez
+        simulations_per_process += self.number_of_processes * [
+            (number_of_simulations_per_process, prediction_window_size)]
+        predictions = pool.starmap(self.simulation_finance, simulations_per_process)
+        return predictions
 
+    def export_finance_file(self, number_of_simulations, prediction_window_size):
+        serial_number = 1
+        if self.parallel_flag == False:
+            predictions = self.mcs_finance_serial(number_of_simulations, prediction_window_size)
+            # r before string converts normal string to raw string
+            path = r"C:\Users\Dule\Desktop\NAPREDNE TEHNIKE PROGRAMIRANJA\PROJEKAT\NTP\Pharo\PythonFinanceSerial.txt"
+            out_file = open(path, "w")
+            for i in range(len(predictions)):
+                for j in range(len(predictions[i])):
+                    if j == 0:
+                        # Write a serial number of simulations at the beginning of a file.
+                        out_file.write(str(serial_number) + "," + " ")
+                    # A component in one row of the output file.
+                    out_file.write(str(predictions[i][j]))
+                    if j == (len(predictions[i]) - 1):
+                        out_file.write("\r\n")
+                        serial_number += 1
+                    else:
+                        out_file.write("," + " ")
+
+            out_file.close()
+
+        else:
+            predictions = self.mcs_finance_parallel(number_of_simulations, prediction_window_size)
+            # r before string converts normal string to raw string
+            path = r"C:\Users\Dule\Desktop\NAPREDNE TEHNIKE PROGRAMIRANJA\PROJEKAT\NTP\Pharo\PythonFinanceParallel.txt"
+            out_file = open(path, "w")
+            for i in range(len(predictions)):
+                for j in range(len(predictions[i])):
+                    for k in range(len(predictions[i][j])):
+                        if k == 0:
+                            # Write a serial number of simulations at the beginning of a file.
+                            out_file.write(str(serial_number) + "," + " ")
+                        # A component in one row of the output file.
+                        out_file.write(str(predictions[i][j][k]))
+                        if k == (len(predictions[i][j]) - 1):
+                            out_file.write("\r\n")
+                            serial_number += 1
+                        else:
+                            out_file.write("," + " ")
+
+
+            out_file.close()
 
 if __name__ == "__main__":
     monte_carlo_simulation_finance = MonteCarloSimulationFinance('1980-01-01', '2019-12-31', 'AAPL', 5)
@@ -99,8 +147,5 @@ if __name__ == "__main__":
     duration = end - start
     print(f"Duration {duration} seconds")
 
-    print(monte_carlo_simulation_finance.calculate_average_daily_return())
-
-
-
+    monte_carlo_simulation_finance.export_finance_file(10, 8)
 
